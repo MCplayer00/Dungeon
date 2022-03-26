5b3e918868295d51eb1545a8746871b43d724ab2
MCplayer00 <10804002@cts.edu.my>
2022-03-26 18:06:06 +0800

    welp

--- scripts/AutoGenerate.gd
+++ scripts/AutoGenerate.gd
@@ -7,12 +7,8 @@ var levels = [preload("res://scenes/auto generates/Flat.tscn"),preload("res://sc
 func _ready() -> void:
 	if Global.level_seed != 0:
 		rng.seed = Global.level_seed
-<<<<<<< Updated upstream
-else: rng.randomize()
-=======
-	else:
-		rng.randomize()
->>>>>>> Stashed changes
+	else: rng.randomize()
+	
 	if OS.has_touchscreen_ui_hint():
 		$"GUI/MB mode/Jump".show()
 	generate_level()
