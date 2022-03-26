a434c697cd639ed4ac6f72b6a0ec40c06981acfb
MCplayer00 <94725241+MCplayer00@users.noreply.github.com>
2022-03-26 13:16:46 +0800

    Update AutoGenerate.gd

--- scripts/AutoGenerate.gd
+++ scripts/AutoGenerate.gd
@@ -7,6 +7,7 @@ var levels = [preload("res://scenes/auto generates/Flat.tscn"),preload("res://sc
 func _ready() -> void:
 	if Global.level_seed != 0:
 		rng.seed = Global.level_seed
+        else: rng.randomize()
 	if OS.has_touchscreen_ui_hint():
 		$"GUI/MB mode/Jump".show()
 	else:
